7b261a57bd10f8df408a2443c0718a53f5f73e5f
AravindR97 <aravindrajeshchry@gmail.com>
2025-03-10 22:43:33 +0530

    feat: Add custom button for Membership

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, Aravind R and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Library Member", {
+	refresh(frm) {
+        frm.add_custom_button("Create Membership", ()=>{
+            frappe.new_doc("Library Membership", {
+                "library_member": frm.doc.name,
+                "from_date": frappe.datetime.get_today()
+            })
+        })
+	},
+});
